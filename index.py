c63c760afff845b901596e46c606b6b2463fb924
MasBr00 <creepyzodiak@gmail.com>
2021-10-19 14:08:48 +0700

    model building

--- index.py
+++ index.py
@@ -84,3 +84,14 @@ y = wine_data['quality'].apply(lambda quality: 1 if quality >= 6 else 0)
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 2)
 print(X.shape,X_train.shape)
 
+print('\n##########\n')
+
+# # Model Building
+
+# penggunaan random forest model
+model = RandomForestClassifier() # deklarasi random forest
+model.fit(X_train,y_train)
+print(model)
+
+print('\n##########\n')
+
